e4cd47a977e00e07c3b81ba3a2c459a614c33389
rayno_mostert <rayno@craftrapps.com>
2017-04-10 19:46:05 +0200

    - Code cleanup for improved readability - adjusted parameters to achieve lower RMSE values.

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,12 +12,8 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
 
-    VectorXd rmse(estimations[0].array().size());
+    VectorXd rmse = VectorXd(estimations[0].array().size());
      rmse.fill(0.0);
-
-      // check the validity of the following inputs: 
-    //  * the estimation vector size should not be zero 
-    //  * the estimation vector size should equal ground truth vector size  
 
     if(estimations.size() != ground_truth.size()  || estimations.size() == 0){ 
         std::cout << "Invalid estimation or ground_truth data" << std::endl; 
@@ -25,7 +21,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     }  
 
     //accumulate squared residuals 
-    for(unsigned int i=0; i < estimations.size(); ++i){  
+    for(unsigned int i = 0; i < estimations.size(); ++i){  
         VectorXd residual = estimations[i] - ground_truth[i];  
 
         //coefficient-wise multiplication 
@@ -33,13 +29,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
         rmse += residual;
     }
 
-      //calculate the mean
-
      rmse = rmse/estimations.size();
-
-      //calculate the squared root
      rmse = rmse.array().sqrt();
-
-      //return the result
     return rmse;
 }
